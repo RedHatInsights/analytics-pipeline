f2a97fd22ff92257bae5794c7ba42baef498cb02
James Tanner <tanner.jc@gmail.com>
2020-10-14 11:57:29 -0400

    wait for clusters page

--- srv/integration_tests/wait_for_stack.sh
+++ srv/integration_tests/wait_for_stack.sh
@@ -12,6 +12,7 @@ fi
 SSO="https://sso.local.redhat.com:8443"
 KCADMIN="http://172.23.0.4"
 CRHC="https://prod.foo.redhat.com:1337"
+CLUSTERS="https://prod.foo.redhat.com:1337/ansible/automation-analytics/clusters"
 
 COUNT=0
 MAXCOUNT=100
@@ -20,19 +21,21 @@ while true; do
     echo "####################################"
     echo "# WAITING FOR SERVICES: $COUNT"
     echo "####################################"
-    curl -v -k --connect-timeout 2 $SSO
+    curl -k -v --connect-timeout 2 $SSO
     SSO_RC=$?
-    curl -v --connect-timeout 2 $KCADMIN
+    curl -k -v --connect-timeout 2 $KCADMIN
     KCADMIN_RC=$?
-    curl -v --connect-timeout 2 $CRHC
+    curl -k -v --connect-timeout 2 $CRHC
     CRHC_RC=$?
+    curl -k -v --connect-timeout 2 $CLUSTERS
+    CLUSTERS_RC=$?
 
     ((COUNT=COUNT+1))
 
-    if [[ $SSO_RC -eq 0 && $KCADMIN_RC -eq 0 && $CHRC_RC -eq 0 ]]; then
+    if [[ $SSO_RC -eq 0 && $KCADMIN_RC -eq 0 && $CHRC_RC -eq 0 && $CLUSTERS_RC -eq 0 ]]; then
         echo "ALL SERVICES ARE AVAILABLE. WAITING 60s ..."
         # wait a bit longer and then exit ...
-        sleep 60
+        sleep 10
         exit 0
     fi
 
@@ -40,6 +43,6 @@ while true; do
         exit 1
     fi
 
-    sleep 5
+    sleep 10
 done
 
